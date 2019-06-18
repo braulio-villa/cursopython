3ccc995508525c930dc2c2e6937c6c3c47733b34
Brauilo <b-alex-villagra@hotmail.com>
2019-06-17 21:05:14 -0600

    clase2

---
+++ clase2.py
@@ -0,0 +1,8 @@
+def resta(a,b):
+    return a-b
+
+def suma(a,b):
+    return a+b
+
+def multiplica(a,b):
+    return(a*b)
---
+++ funciones.py
@@ -0,0 +1,8 @@
+import clase2
+
+a = int(input("1.suma \n")
+
+if a == 1:
+    print(clase2.suma(30,10))
+
+    
